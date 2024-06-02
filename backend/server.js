5e853499dbee7b11367940d07cb0b66499f88483
manojmksmanu <manoj2022019@gmail.com>
2024-06-02 15:46:05 +0530

    chitchat mern app setup

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const { chats } = require("./data/data");
+const dotenv = require("dotenv");
+const app = express();
+dotenv.config();
+
+app.get("/", (req, res) => {
+  res.send("api is running");
+});
+app.get("/api/chat", (req, res) => {
+  res.send(chats);
+});
+app.get("/api/chat/:id", (req, res) => {
+  console.log(req.params.id);
+  const singlechat = chats.find((c) => c._id === req.params.id);
+  res.send(singlechat);
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`server started on port ${PORT}`));
