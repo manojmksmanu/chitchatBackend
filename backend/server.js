5c25b4b80ded92d96a1507190f7af762d008e1e1
manojmksmanu <manoj2022019@gmail.com>
2024-08-08 20:03:35 +0530

    add live frontend url to socket origin

--- backend/server.js
+++ backend/server.js
@@ -52,7 +52,7 @@ const server = app.listen(5000, () => {
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://chit-chat-newfrontend.vercel.app/",
   },
 });
 
